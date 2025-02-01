1e22b2a3a5d77cea1b656ae652dca3e4d2c0a509
jncfa <jncfa@proton.me>
2025-02-01 19:03:07 +0100

    add basic sentry wrapper

--- src/sentry_ros2_test/src/sentry_test.cpp
+++ src/sentry_ros2_test/src/sentry_test.cpp
@@ -1,17 +1,29 @@
 #include <sentry.h>
 #include <cstring>
+#include <stdio.h>
+
+void print_envelope(sentry_envelope_t *envelope, void *unused_state)
+{
+    (void)unused_state;
+    size_t size_out = 0;
+    char *s = sentry_envelope_serialize(envelope, &size_out);
+    printf("%s", s);
+    sentry_free(s);
+    sentry_envelope_free(envelope);
+}
+
 int main(void) {
   sentry_options_t *options = sentry_options_new();
-  sentry_options_set_dsn(options, "https://9527a7b54370e34942f523ba74ff9b69@o4508315891662848.ingest.de.sentry.io/4508316055371856");
+  sentry_options_set_dsn(options, "https://1f47648a187289af2453f90b97a75a98@o4508315891662848.ingest.de.sentry.io/4508723663536208");
   // This is also the default-path. For further information and recommendations:
   // https://docs.sentry.io/platforms/native/configuration/options/#database-path
   sentry_options_set_database_path(options, ".sentry-native");
-  sentry_options_set_release(options, "sentry_ros2_test@2.3.12");
+  sentry_options_set_release(options, "native@0.0.1");
   sentry_options_set_debug(options, 1);
   sentry_options_set_handler_path(options, "/workspaces/ros2-sentry/install/opt/sentry_vendor/bin/crashpad_handler");
+
   sentry_init(options);
 
-  /* ... */
   memset((void*)(intptr_t)4, 123, 1);
 
   // make sure everything flushes
---
+++ src/sentry_vendor/sentry_vendor-extras.cmake
@@ -0,0 +1,4 @@
+list(INSERT CMAKE_MODULE_PATH 0 "${sentry_vendor_DIR}/Modules")
+
+#list(APPEND CMAKE_MODULE_PATH "${sentry_vendor_DIR}/../../../opt/sentry_vendor/lib/cmake/sentry")
+#find_package(sentry REQUIRED CONFIG)
